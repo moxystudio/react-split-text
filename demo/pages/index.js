a247413ff3fd24ebdbee5db5a6cfcacd0de5c7d6
Diogo Silva <fsdiogo@gmail.com>
2020-04-23 15:48:37 +0100

    feat: add SplitText component

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import NewComponent from '@moxy/react-lib-template';
-
+import SplitText from '@moxy/react-split-text';
 import styles from './index.module.css';
 
 const Home = () => (
     <div className={ styles.home }>
         <h1>react-lib-template</h1>
-        <NewComponent className={ styles.newComponent }>Hello World</NewComponent>
+
+        <div className={ styles.textWrapper }>
+            <SplitText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </SplitText>
+            <span className={ styles.final }>BYE BYE!</span>
+        </div>
+
+        <div className={ styles.textWrapper }>
+            <SplitText separator={ '-' }>
+                Lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit,-sed-do-eiusmod-tempor-incididunt-ut-labore-et-dolore-magna-aliqua.
+            </SplitText>
+            <span className={ styles.final }>BYE BYE!</span>
+        </div>
     </div>
 );
 
